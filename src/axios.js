9215d230a75abd0d6ca7d6f57494e8336c2a5b35
wei.liu <wei.liu@vechain.com>
2019-01-08 19:04:44 +0800

    update axios

--- src/axios.js
+++ src/axios.js
@@ -9,8 +9,8 @@ axios.defaults.withCredentials = true
 
 // 动态配置接口域名
 if (process.env.NODE_ENV === 'production') {
-  Vue.prototype.baseURL = 'http://192.168.28.136:3300/'
-  axios.defaults.baseURL = 'http://192.168.28.136:3300/'
+  Vue.prototype.baseURL = 'https://innovation-manage-dev.vechaindev.com/'
+  axios.defaults.baseURL = 'https://innovation-manage-dev.vechaindev.com/'
 } else {
   Vue.prototype.baseURL = 'http://192.168.10.47:3000/'
   axios.defaults.baseURL = ''
