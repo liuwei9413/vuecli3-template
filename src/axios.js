d71d7230b2e538c139fd3517c0e519cb17beaece
wei.liu <wei.liu@vechain.com>
2019-01-08 17:21:02 +0800

    update axios

--- src/axios.js
+++ src/axios.js
@@ -9,7 +9,7 @@ axios.defaults.withCredentials = true
 
 // 动态配置接口域名
 if (process.env.NODE_ENV === 'production') {
-  Vue.prototype.baseURL = 'http://192.168.28.136:3301/'
+  Vue.prototype.baseURL = 'http://192.168.28.136:3300/'
   axios.defaults.baseURL = ''
 } else {
   Vue.prototype.baseURL = 'http://192.168.10.47:3000/'
