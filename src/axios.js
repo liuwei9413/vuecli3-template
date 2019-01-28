1ed970cd368e5690cf59e3a4f80931544a4e93b4
wei.liu <wei.liu@vechain.com>
2019-01-28 16:40:26 +0800

    fix axios config

--- src/axios.js
+++ src/axios.js
@@ -18,15 +18,10 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 axios.interceptors.request.use((config) => {
-  // config.headers['access-token'] = getToken() // 每个请求都带上token
   config.params = config.params
     ? config.params
     : {}
   return config
-  // if (config.method === 'post') { // POST传参序列化
-  //   config.data = qs.stringify(config.data)
-  // }
-  // return config
 }, (error) => {
   console.log('参数错误')
   return Promise.reject(error)
@@ -42,11 +37,10 @@ axios.interceptors.response.use(
   },
   error => {
     const response = error.response
-    // console.log(response)
     if (response && Object.prototype.toString.call(response.data) === '[object Object]') {
       // token非法 跳到登录页
-      if (response.data.code === 1005) {
-        removeStorage('username')
+      if (response.data.code === 10002) {
+        removeStorage('userInfo')
         location.href = '/#/login'
       } else if (response.data.code === 10015) {
         // 不提示错误
